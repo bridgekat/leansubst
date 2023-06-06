3ab3b7ae4041654e53c726dc59bb3f7a0da2942b
bridgekat <zhanrong.qiao21@imperial.ac.uk>
2023-06-07 00:29:05 +0100

    Simplify

---
+++ Leansubst/Basic.lean
@@ -0,0 +1,262 @@
+import Std.Data.Nat.Lemmas
+import Leansubst.Defs
+
+/-!
+  This file contains equational lemmas related to `Expr`, `Subst`, `ES` and `SE`.
+-/
+
+namespace Leansubst.Subst'
+
+variable {σ : Type}
+variable (s t u : Subst σ)
+variable (i : Nat) (x : σ)
+variable (e : Expr σ) (es : List (Expr σ))
+
+local notation:arg "⟦" a "⟧" => Quotient.mk _ a
+
+def up : Nat → Subst' σ → Subst' σ
+  | 0,     s => s
+  | i + 1, s => cons (.var 0) (comp (up i s) (shift 1))
+
+theorem get_up_low (i j k : Nat) (h : k < j) : (up j (@shift σ i)).get k = .var k := by
+  induction j generalizing k with
+  | zero => intros; contradiction
+  | succ j ih =>
+    cases Nat.eq_or_lt_of_le (Nat.le_of_lt_succ h) with
+    | inl h' =>
+      subst h'; rw [up]
+      cases k with
+      | zero => rfl
+      | succ k => rw [get, comp_def, ih k (Nat.lt_succ_self _), apply]
+    | inr h' =>
+      rw [up]
+      cases k with
+      | zero => rfl
+      | succ k =>
+        specialize ih _ (Nat.le_of_lt_succ h)
+        rw [get, comp_def, ih, apply]
+
+theorem get_up_high (i j k : Nat) (h : j ≤ k) : (up j (@shift σ i)).get k = .var (k + i) := by
+  have ⟨d, hd⟩ := Nat.le.dest h; clear h
+  induction j generalizing k d with
+  | zero => rfl
+  | succ j ih =>
+    rw [up]
+    cases k with
+    | zero => cases d <;> contradiction
+    | succ k =>
+      rw [get, comp_def]
+      rw [Nat.succ_add] at hd; injection hd with hd
+      rw [ih _ _ hd, apply, Nat.succ_add]
+
+theorem comp_eqv_comps (i j : Nat) : comp (up j (shift i)) (shift 1) ≈ (comps (up j (shift i)) 1 : Subst' σ) := by
+  intros k
+  rw [comp_def, comps_def]
+  cases Nat.lt_sum_ge k j with
+  | inl h =>
+    rw [get_up_low _ _ _ h, apply, applys]
+    split
+    case inl _ => rfl
+    case inr h => exact (h (Nat.zero_le _)).elim
+  | inr h =>
+    rw [get_up_high _ _ _ h, apply, applys]
+    split
+    case inl _ => rfl
+    case inr h => exact (h (Nat.zero_le _)).elim
+
+/-- We have to show that `applys` actually agrees with `apply shift`. -/
+theorem applys_def (i j : Nat) : applys j i e = apply (up j (shift i)) e := by
+  revert j
+  -- Induction on `e`.
+  apply @Expr.recOn _
+    (fun e => ∀ j, applys j i e = apply (up j (shift i)) e)
+    (List.foldr (fun e etc => (∀ j, applys j i e = apply (up j (shift i)) e) ∧ etc) True)
+    <;> intros <;> try trivial
+  case var k j =>
+    induction j generalizing k with
+    | zero =>
+      rw [up, applys]
+      split
+      case inl _ => rw [apply]
+      case inr h => exact (h (Nat.zero_le _)).elim
+    | succ j ih =>
+      simp only [up, applys, apply_var] at *
+      cases k with
+      | zero => (split <;> try contradiction); rfl
+      | succ k =>
+        simp only [get, comp_def, ← ih]
+        split <;> rename_i h
+        rotate_right; have h : ¬j ≤ k := fun h' => (h (Nat.succ_le_succ h')).elim
+        rotate_right; have h := Nat.le_of_succ_le_succ h
+        all_goals (split <;> try contradiction)
+        case inl => rw [apply, Nat.succ_add]
+        case inr => rw [apply]
+  case binder e ih j =>
+    rw [applys, apply, ih, up]
+    congr 1
+    have h := comp_eqv_comps i j (σ := σ)
+    have h' : (.cons (.var 0) ⟦comp (up j (shift i)) (shift 1)⟧ : Subst σ) e =
+              (.cons (.var 0) ⟦comps (up j (shift i)) 1⟧ : Subst σ) e := by rw [Quotient.sound h]
+    exact h'
+  case node x es ih j =>
+    induction es with
+    | nil => rw [applys, apply, apply.nested]; rfl
+    | cons h' t' ih' =>
+      rw [List.foldr] at ih
+      rw [applys, applys.nested, apply, apply.nested]
+      congr
+      . exact ih.left _
+      . specialize ih' ih.right
+        rw [applys, apply] at ih'
+        injection ih'
+
+end Subst'
+
+namespace Subst
+
+@[simp] theorem shift_wrap (n : Nat) : ⟦Subst'.shift n⟧ = Subst.shift σ n := rfl
+@[simp] theorem cons_wrap (e : Expr σ) (s : Subst' σ) : ⟦Subst'.cons e s⟧ = Subst.cons e ⟦s⟧ := rfl
+@[simp] theorem id_wrap : ⟦Subst'.id σ⟧ = Subst.id σ := rfl
+
+@[simp] theorem get_wrap (s : Subst' σ) (i : Nat) : Subst'.get s i = Subst.get ⟦s⟧ i := rfl
+@[simp] theorem apply_wrap (s : Subst' σ) : Subst'.apply s = Subst.apply ⟦s⟧ := rfl
+@[simp] theorem comps_wrap (s : Subst' σ) (n : Nat) : ⟦Subst'.comps s n⟧ = Subst.comps ⟦s⟧ n := rfl
+@[simp] theorem comp_wrap (s t : Subst' σ) : ⟦Subst'.comp s t⟧ = Subst.comp ⟦s⟧ ⟦t⟧ := rfl
+
+variable {σ : Type}
+variable (s t u : Subst σ)
+variable (i : Nat) (x : σ)
+variable (e : Expr σ) (es : List (Expr σ))
+
+@[simp]
+theorem get_zero : (cons e s).get 0 = e := by
+  induction s using Quotient.inductionOn
+  rw [← cons_wrap, ← get_wrap]
+  rfl
+
+@[simp]
+theorem get_succ : (cons e s).get (i + 1) = s.get i := by
+  induction s using Quotient.inductionOn
+  rw [← cons_wrap, ← get_wrap]
+  rfl
+
+@[simp]
+theorem shift_expand : shift σ (i + 1 + 1) = comp (shift σ (i + 1)) (shift σ 1) := by
+  simp only [← shift_wrap, ← comp_wrap]
+  apply Quotient.sound
+  intros j
+  rfl
+
+@[simp]
+theorem comps_expand : comps s i = comp s (shift σ i) := by
+  induction s using Quotient.inductionOn
+  rw [← comps_wrap, ← shift_wrap, ← comp_wrap]
+  apply Quotient.sound
+  intros j
+  rw [Subst'.comps_def, Subst'.comp_def, Subst'.applys_def]
+  rfl
+
+@[simp]
+theorem apply_var : s (.var i) = s.get i := by
+  induction s using Quotient.inductionOn
+  rw [← apply_wrap, ← get_wrap, Subst'.apply_var]
+
+@[simp]
+theorem apply_binder : s (.binder e) = .binder ((cons (.var 0) (comp s (shift σ 1))) e) := by
+  induction s using Quotient.inductionOn
+  rw [← apply_wrap, Subst'.apply]
+  simp
+
+@[simp]
+theorem apply_node : s (.node x es) = .node x (es.map s) := by
+  induction s using Quotient.inductionOn
+  induction es with
+  | nil => rw [List.map, ← apply_wrap, Subst'.apply, Subst'.apply.nested]
+  | cons h t ih =>
+    rw [List.map, ← apply_wrap, Subst'.apply, Subst'.apply.nested]
+    rw [← apply_wrap, Subst'.apply] at ih
+    congr; injection ih
+
+@[simp]
+theorem apply_apply : t (s e) = (comp s t) e := by
+  induction s using Quotient.inductionOn; rename_i s
+  induction t using Quotient.inductionOn; rename_i t
+  simp only [← comp_wrap, ← apply_wrap]
+  sorry
+
+@[simp]
+theorem id_apply : (id σ) e = e := by
+  rw [← id_wrap, ← apply_wrap]
+  rw [Subst'.id]
+  sorry
+
+@[simp]
+theorem id_comp : comp (id σ) s = s := by
+  induction s using Quotient.inductionOn
+  rw [← id_wrap, ← comp_wrap]
+  apply Quotient.sound
+  intros j
+  rw [Subst'.id, Subst'.comp_def, Subst'.get, Subst'.apply_var]
+  rfl
+
+@[simp]
+theorem comp_id : comp s (id σ) = s := by
+  induction s using Quotient.inductionOn
+  rw [← id_wrap, ← comp_wrap]
+  apply Quotient.sound
+  intros j
+  rw [Subst'.comp_def]
+  sorry
+
+@[simp]
+theorem shift_comp_cons : comp (shift σ 1) (cons e s) = s := by
+  induction s using Quotient.inductionOn
+  rw [← cons_wrap, ← shift_wrap, ← comp_wrap]
+  apply Quotient.sound
+  intros j
+  rw [Subst'.comp_def, Subst'.get, Subst'.apply, Subst'.apply_var]
+
+@[simp]
+theorem cons_comp : comp (cons e s) t = cons (s e) (comp s t) := by
+  induction s using Quotient.inductionOn
+  induction t using Quotient.inductionOn
+  simp only [← apply_wrap, ← cons_wrap, ← comp_wrap]
+  apply Quotient.sound
+  intros j
+  sorry
+
+@[simp]
+theorem comp_assoc : comp (comp s t) u = comp s (comp t u) := by
+  induction s using Quotient.inductionOn
+  induction t using Quotient.inductionOn
+  induction u using Quotient.inductionOn
+  simp only [← comp_wrap]
+  apply Quotient.sound
+  intros j
+  simp only [Subst'.comp_def, Subst'.apply]
+  sorry
+
+@[simp]
+theorem apply_zero_cons_shift_comp : cons (s (.var 0)) (comp (shift σ 1) s) = s := by
+  induction s using Quotient.inductionOn
+  rw [← shift_wrap, ← apply_wrap, ← comp_wrap, ← cons_wrap]
+  apply Quotient.sound
+  intros j
+  rw [Subst'.apply_var]
+  sorry
+
+@[simp]
+theorem zero_cons_shift : cons (.var 0) (shift σ 1) = id σ := by
+  rw [← shift_wrap, ← id_wrap, ← cons_wrap]
+  apply Quotient.sound
+  intros j
+  cases j with
+  | zero => rfl
+  | succ => rfl
+
+/-
+example : comp (shift σ 1) (id σ) = shift σ 1 := by
+  simp
+-/
+
+end Leansubst.Subst
--- Leansubst/Defs.lean
+++ Leansubst/Defs.lean
@@ -1,109 +1,231 @@
 import Std.Data.Nat.Basic
 import Std.Tactic.Basic
 
+/-!
+  This file contains the definitions of:
+
+  - Expressions: `Expr`;
+  - Substitutions: `Subst` (as a quotient of regular substitutions modulo extensional equivalence);
+  - Expressions with explicit substitutions and "metavariables": `ES` and `SE`.
+
+  The development roughly follows
+  [[1]](https://www.ps.uni-saarland.de/Publications/documents/SchaeferEtAl_2015_Completeness.pdf)
+  with some difference in terminology and formalism.
+-/
+
 namespace Leansubst
 
-/-! First we define expressions without explicit substitutions and "metavariables".
-    They are also a semantic model for more complex expressions (so-called "de Bruijn algebra"). -/
+/-!
+  First we define expressions *without* explicit substitutions and "metavariables".
+  They serve as a semantic model for more complex expressions (so-called "de Bruijn algebra").
+-/
 
 /-- Abstract syntax tree using de Bruijn indices. -/
 inductive Expr (σ : Type) where
   | var    : Nat →               Expr σ -- Variable in de Bruijn index.
   | binder : Expr σ →            Expr σ -- Binder presence.
   | node   : σ → List (Expr σ) → Expr σ -- Tree node.
--- Nested inductive types (e.g. the `List Expr` here) are a special case of mutual inductive types.
-
-/-- Renamings. -/
-inductive Renaming where
-  | shift : Nat →            Renaming -- Shift overflow variables up.
-  | cons  : Nat → Renaming → Renaming -- Prepend to sequence.
-
-namespace Renaming
-
-/-- The identity renaming. -/
-def id : Renaming :=
-  shift 0
-
-/-- Evaluate renaming at a given index. -/
-def get : Renaming → Nat → Nat
-  | shift n,  i     => i + n
-  | cons h _, 0     => h
-  | cons _ t, i + 1 => get t i
-
-/-- Composition of renamings. -/
-def comp : Renaming → Renaming → Renaming
-  | shift n,       shift m  => shift (n + m)
-  | shift 0,       cons h t => cons h t
-  | shift (n + 1), cons _ t => comp (shift n) t
-  | cons h t,      r        => cons (r.get h) (comp t r)
-
-/-- Applies a renaming on a term. -/
-def apply {σ} : Renaming → Expr σ → Expr σ
-  | r, .var i     => .var $ r.get i
-  | r, .binder e  => .binder $ apply (cons 0 (comp r (shift 1))) e
-  | r, .node n es => .node n $ applies r es
-where applies {σ} : Renaming → List (Expr σ) → List (Expr σ)
-  | _, []      => []
-  | r, e :: es => apply r e :: applies r es
-termination_by
-  apply _ e    => sizeOf e
-  applies _ es => sizeOf es
-
-end Renaming
 
 /-- Substitutions. -/
-inductive Subst (σ : Type) where
-  | shift : Nat →              Subst σ -- Shift overflow variables up.
-  | cons  : Expr σ → Subst σ → Subst σ -- Prepend to sequence.
+inductive Subst' (σ : Type) where
+  | shift : Nat →               Subst' σ -- Shift overflow variables up.
+  | cons  : Expr σ → Subst' σ → Subst' σ -- Prepend to sequence.
 
-namespace Subst
-
-def fromRenaming (σ) : Renaming → Subst σ
-  | .shift n  => shift n
-  | .cons h t => cons (.var h) (fromRenaming σ t)
-
-instance {σ} : Coe Renaming (Subst σ) :=
-  ⟨fromRenaming σ⟩
-
-/-- The identity substitution. -/
-def id (σ) : Subst σ :=
-  shift 0
+namespace Subst'
 
 /-- Evaluate substitution at a given index. -/
-def get {σ} : Subst σ → Nat → Expr σ
+def get : Subst' σ → Nat → Expr σ
   | shift n,  i     => .var (i + n)
   | cons h _, 0     => h
   | cons _ t, i + 1 => get t i
 
-/-- Composition of substitution and renaming. -/
-def compr {σ} : Subst σ → Renaming → Subst σ
-  | shift n,  r => Renaming.comp (.shift n) r
-  | cons h t, r => cons (r.apply h) (compr t r)
+/-- Extensional equivalence of substitutions. -/
+def eqv (s : Subst' σ) (t : Subst' σ) : Prop :=
+  ∀ i, s.get i = t.get i
+
+theorem eqv.refl (s : Subst' σ) : eqv s s :=
+  fun _ => .refl _
+
+theorem eqv.symm {s t : Subst' σ} : eqv s t → eqv t s :=
+  fun h i => .symm (h i)
+
+theorem eqv.trans {s t u : Subst' σ} : eqv s t → eqv t u → eqv s u :=
+  fun h₁ h₂ i => .trans (h₁ i) (h₂ i)
+
+instance setoid (σ) : Setoid (Subst' σ) where
+  r     := eqv
+  iseqv := ⟨eqv.refl, eqv.symm, eqv.trans⟩
+
+/-- The identity substitution. -/
+def id (σ) : Subst' σ :=
+  shift 0
+
+/-- Shift variables with level ≥ `n` by `m` levels. -/
+def applys : Nat → Nat → Expr σ → Expr σ
+  | n, m, .var i     => if n <= i then .var (i + m) else .var i
+  | n, m, .binder e  => .binder (applys (n + 1) m e)
+  | n, m, .node x es => .node x (nested n m es)
+where nested : Nat → Nat → List (Expr σ) → List (Expr σ)
+  | _, _, []      => []
+  | n, m, e :: es => applys n m e :: nested n m es
+
+/-- Composition of substitution and shift. -/
+def comps : Subst' σ → Nat → Subst' σ
+  | shift n,  m => .shift (n + m)
+  | cons h t, m => cons (applys 0 m h) (comps t m)
+
+/-- Composition of substitution and shift follows the expected definition. -/
+theorem comps_def (s : Subst' σ) (n : Nat) : ∀ i, (comps s n).get i = applys 0 n (s.get i) := by
+  intros i
+  induction s generalizing n i with
+  | shift m =>
+    simp only [get, comps, applys]
+    split
+    case inl => rw [Nat.add_assoc]
+    case inr h => exfalso; exact h (Nat.zero_le _)
+  | cons h s ih =>
+    cases i with
+    | zero => simp only [get, ih]
+    | succ i => simp only [get, ih]
 
 /-- Applies a substitution on a term. -/
-def apply {σ} : Subst σ → Expr σ → Expr σ
-  | shift n,     .var i       => .var (i + n)
-  | cons h _,    .var 0       => h
-  | cons _ t,    .var (i + 1) => apply t (.var i)
-  | s,           .binder e    => .binder (apply (cons (.var 0) (compr s (.shift 1))) e)
-  | s,           .node n es   => .node n (applies s es)
-where applies {σ} : Subst σ → List (Expr σ) → List (Expr σ)
+def apply : Subst' σ → Expr σ → Expr σ
+  | shift n,  .var i       => .var (i + n)
+  | cons h _, .var 0       => h
+  | cons _ t, .var (i + 1) => apply t (.var i)
+  | s,        .binder e    => .binder (apply (cons (.var 0) (comps s 1)) e)
+  | s,        .node n es   => .node n (nested s es)
+where nested : Subst' σ → List (Expr σ) → List (Expr σ)
   | _, []      => []
-  | s, e :: es => apply s e :: applies s es
+  | s, e :: es => apply s e :: nested s es
 termination_by
-  apply s e    => (sizeOf e, sizeOf s)
-  applies s es => (sizeOf es, sizeOf s)
+  apply s e   => (sizeOf e, sizeOf s)
+  nested s es => (sizeOf es, sizeOf s)
+
+/-- Applying a substitution on a variable is equal to `get`. -/
+theorem apply_var (s : Subst' σ) : ∀ i, s.apply (.var i) = s.get i := by
+  intros i
+  induction s generalizing i with
+  | shift n => simp only [apply, get]
+  | cons h t ih =>
+    cases i with
+    | zero => simp [apply, get]
+    | succ i => simp only [apply, get, ih]
 
 /-- Composition of substitutions. -/
-def comp {σ} : Subst σ → Subst σ → Subst σ
+def comp : Subst' σ → Subst' σ → Subst' σ
   | shift n,       shift m  => shift (n + m)
   | shift 0,       cons h t => cons h t
   | shift (n + 1), cons _ t => comp (shift n) t
   | cons h t,      r        => cons (r.apply h) (comp t r)
 
+/-- Composition of substitutions follows the expected definition. -/
+theorem comp_def (s t : Subst' σ) : ∀ i, (comp s t).get i = t.apply (s.get i) := by
+  intros i
+  induction s generalizing t i with
+  | shift n =>
+    induction t generalizing n with
+    | shift m => simp only [get, comp, apply_var]; rw [Nat.add_assoc]
+    | cons h t ih =>
+      cases n with
+      | zero => simp [get, comp, apply_var]
+      | succ n => simp [get, comp, apply_var, ih]
+  | cons h s ih =>
+    cases i with
+    | zero => simp only [get, comp, ih]
+    | succ i => simp only [get, comp, ih]
+
+/-- Now we can write `s t` for applying substitution `s` on term `t`. -/
+instance : CoeFun (Subst' σ) (fun _ => Expr σ → Expr σ) where
+  coe := apply
+
+end Subst'
+
+/-- Substitution modulo extensional equivalence. -/
+def Subst (σ : Type) : Type :=
+  Quotient (Subst'.setoid σ)
+
+namespace Subst
+
+scoped notation:arg "⟦" a "⟧" => Quotient.mk _ a
+
+/-- Evaluate substitution at a given index. -/
+def get : Subst σ → Nat → Expr σ :=
+  Quotient.lift Subst'.get $
+    fun _ _ h => funext $ fun i => h i
+
+/-- Lifted constructor `shift`. -/
+def shift (σ) : Nat → Subst σ :=
+  fun n => ⟦Subst'.shift n⟧
+
+/-- Lifted constructor `cons`. -/
+def cons : Expr σ → Subst σ → Subst σ :=
+  fun e => Quotient.lift (fun s => ⟦Subst'.cons e s⟧) $ by
+    intros s t h; apply Quotient.sound; intros i
+    cases i with
+    | zero => eq_refl
+    | succ i => exact h i
+
+/-- Lifted constructor `id`. -/
+def id (σ) : Subst σ :=
+  ⟦Subst'.id σ⟧
+
+/-- Composition of substitution and shift. -/
+def comps : Subst σ → Nat → Subst σ :=
+  Quotient.lift (fun s n => ⟦Subst'.comps s n⟧) $ by
+    intros s t h
+    apply funext
+    intros n
+    apply Quotient.sound
+    intros i
+    simp [Subst'.comps_def, h i]
+
+/-- Applies a substitution on a term. -/
+def apply : Subst σ → Expr σ → Expr σ :=
+  Quotient.lift Subst'.apply $ by
+    intros s t h; apply funext; intros e; revert s t
+    -- Induction on `e`.
+    apply @Expr.recOn _
+      (fun e => ∀ s t : Subst' σ, s ≈ t → s.apply e = t.apply e)
+      (List.foldr (fun e etc => (∀ s t : Subst' σ, s ≈ t → s.apply e = t.apply e) ∧ etc) True)
+      <;> intros <;> try trivial
+    case var i s t h => simp only [Subst'.apply_var, h i]
+    case binder e' ih s t h =>
+      simp [Subst'.apply]
+      apply ih
+      apply Quotient.exact
+      suffices h : cons (.var 0) (comps ⟦s⟧ 1) = cons (.var 0) (comps ⟦t⟧ 1) by exact h
+      suffices h : ⟦s⟧ = ⟦t⟧ by rw [h]
+      exact Quotient.sound h
+    case node n es ih s t h =>
+      simp [Subst'.apply]
+      induction es with
+      | nil => simp only [Subst'.apply.nested]
+      | cons h' t' ih' =>
+        simp only [List.foldr] at ih
+        simp [Subst'.apply.nested]
+        exact ⟨ih.left _ _ h, ih' ih.right⟩
+
+/-- Composition of substitutions. -/
+def comp : Subst σ → Subst σ → Subst σ :=
+  Quotient.lift₂ (fun s t => ⟦Subst'.comp s t⟧) $ by
+    intros s₁ t₁ s₂ t₂ h₁ h₂
+    apply Quotient.sound
+    intros i
+    simp [Subst'.comp_def]
+    suffices h : Subst.apply ⟦t₁⟧ (get ⟦s₁⟧ i) = Subst.apply ⟦t₂⟧ (get ⟦s₂⟧ i) by exact h
+    suffices h : ⟦s₁⟧ = ⟦s₂⟧ ∧ ⟦t₁⟧ = ⟦t₂⟧ by rw [h.left, h.right]
+    exact ⟨Quotient.sound h₁, Quotient.sound h₂⟩
+
+/-- Now we can write `s t` for applying substitution `s` on term `t`. -/
+instance : CoeFun (Subst σ) (fun _ => Expr σ → Expr σ) where
+  coe := apply
+
 end Subst
 
-/-! Now define expressions **with** explicit substitutions and "metavariables". -/
+/-!
+  Now define expressions *with* explicit substitutions and "metavariables".
+-/
 
 mutual
 /-- Explicit substitutions using de Bruijn indices. -/
@@ -134,22 +256,22 @@ def SE.id (σ) : SE σ :=
 /-- Metavariable assignment. -/
 structure Assignment (σ : Type) where
   es : Nat → Expr σ
-  se : Nat → Subst σ
+  se : Nat → Subst' σ
 
 mutual
 /-- Assign metavariables. -/
-def ES.assign {σ} : Assignment σ → ES σ → Expr σ
-  | a, .subst e s => Subst.apply (SE.assign a s) (ES.assign a e)
+def ES.assign : Assignment σ → ES σ → Expr σ
+  | a, .subst e s => Subst'.apply (SE.assign a s) (ES.assign a e)
   | _, .zero      => .var 0
   | a, .binder e  => .binder (ES.assign a e)
-  | a, .node n es => .node n (ES.assigns a es)
+  | a, .node n es => .node n (nested a es)
   | a, .mvar i    => a.es i
-where ES.assigns {σ} : Assignment σ → List (ES σ) → List (Expr σ)
+where nested : Assignment σ → List (ES σ) → List (Expr σ)
   | _, []         => []
-  | a, e :: es    => ES.assign a e :: ES.assigns a es
+  | a, e :: es    => ES.assign a e :: nested a es
 
 /-- Assign metavariables. -/
-def SE.assign {σ} : Assignment σ → SE σ → Subst σ
+def SE.assign : Assignment σ → SE σ → Subst' σ
   | _, .shift     => .shift 1
   | a, .cons h t  => .cons (ES.assign a h) (SE.assign a t)
   | a, .comp s t  => .comp (SE.assign a s) (SE.assign a t)
