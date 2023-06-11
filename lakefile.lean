3d464dbb97fa3a4d0db547ad54cda70c21d4e585
bridgekat <zhanrong.qiao21@imperial.ac.uk>
2023-06-11 02:40:25 +0100

    Rewriting system working

--- lakefile.lean
+++ lakefile.lean
@@ -1,8 +1,11 @@
 import Lake
 open Lake DSL
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4"
+-- require mathlib from git
+--   "https://github.com/leanprover-community/mathlib4"
+
+require std4 from git
+  "https://github.com/leanprover/std4"@"d5471b83378e8ace4845f9a029af92f8b0cf10cb"
 
 package «leansubst» {
   -- add package configuration options here
